ab4241e63d093966ed9afe2a86f1055ab60e4715
Dmitrij Zherebjarjew <zherebjatjew@gmail.com>
2013-03-31 22:31:26 +0700

    Add Nested vectored interrupt controlled dependency

--- timers.c
+++ timers.c
@@ -2,7 +2,7 @@
 #include <stm32f4xx.h>
 #include <stm32f4xx_tim.h>
 #include <stm32f4xx_rcc.h>
-//#include <misc.h>
+#include <misc.h>
 #include <stdlib.h>
 
 //////////////////////////////////////////////////////////////////
